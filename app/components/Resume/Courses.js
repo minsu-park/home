4e2ff7dd102796671717b9b470355af8484a8a22
minsu-park <likeateenspirit@gmail.com>
2021-12-13 11:54:49 +0400

    updates

--- app/components/Resume/Courses.js
+++ app/components/Resume/Courses.js
@@ -8,7 +8,7 @@ const Courses = () => (
       <a href="https://minsu-park.github.io/CV_MinsuPark.pdf">
       <h3>FULL CV (CLICK)</h3>
       </a>
-      <h5>See this full curriculum vitae for more details of <br/>
+      <h5>See this full curriculum vitae for more details of
       my qualifications, employment experience, and publications.</h5>
     </div>
   </div>
